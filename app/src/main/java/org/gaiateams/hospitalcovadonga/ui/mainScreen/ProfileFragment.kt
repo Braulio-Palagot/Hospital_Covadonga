8b8e6d15e52b6382dca904b484d9cf6440a3aab6
Braulio Hernández Palagot <bdpalagot@gmail.com>
2021-12-03 10:45:55 -0600

    Implementación de Logout
    Cambios Realizados:
        1) Se agregó un botón  de cierre de sesión en la ventana del perfil de usuario.
        2) Se agregó la funcionalidad de cierre de sesión mediante las SharedPreferences.
        3) Al cerrar sesión se cierra la activity main y se abre la activity authentication.

--- app/src/main/java/org/gaiateams/hospitalcovadonga/ui/mainScreen/ProfileFragment.kt
+++ app/src/main/java/org/gaiateams/hospitalcovadonga/ui/mainScreen/ProfileFragment.kt
@@ -1,11 +1,17 @@
 package org.gaiateams.hospitalcovadonga.ui.mainScreen
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.gaiateams.hospitalcovadonga.AuthenticationActivity
+import org.gaiateams.hospitalcovadonga.MainActivity
 import org.gaiateams.hospitalcovadonga.databinding.FragmentProfileBinding
+import org.gaiateams.hospitalcovadonga.utils.IS_LOGGED
+import org.gaiateams.hospitalcovadonga.utils.SAVED_USER
+import org.gaiateams.hospitalcovadonga.utils.preferences
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
@@ -16,6 +22,19 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.bttnLogout.setOnClickListener {
+            val preferencesEditor = preferences.edit()
+            preferencesEditor.putBoolean(
+                IS_LOGGED,
+                false
+            )
+            preferencesEditor.apply()
+
+            val logout = Intent(activity, AuthenticationActivity::class.java)
+            startActivity(logout)
+            (activity as MainActivity).finish()
+        }
 
         return binding.root
     }
